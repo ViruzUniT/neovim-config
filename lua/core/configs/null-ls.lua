0f8fe18bf888a6ed2832b44b4ec6de4883dbd1f9
ViruzUniT <73714544+ViruzUniT@users.noreply.github.com>
2024-03-22 00:09:19 +0100

    Added rust and fixed some bugs

--- lua/core/configs/null-ls.lua
+++ lua/core/configs/null-ls.lua
@@ -6,8 +6,8 @@ local b = null_ls.builtins
 local opts = {
   sources = {
     -- webdev stuff
-    b.formatting.deno_fmt,                                                      -- choosed deno for ts/js files cuz its very fast!
-    b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } },   -- so prettier works only on these filetypes
+    b.formatting.deno_fmt,                                                    -- choosed deno for ts/js files cuz its very fast!
+    b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
 
     -- Lua
     b.formatting.stylua,
