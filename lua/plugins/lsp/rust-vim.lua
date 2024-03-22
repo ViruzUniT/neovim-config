fbb863f4ef6ccca609ba295b247e59af4c5aae9f
ViruzUniT <viruz.unit@gmail.com>
2024-03-22 01:08:54 +0100

    Some modifications for rust to work

--- lua/plugins/lsp/rust-vim.lua
+++ lua/plugins/lsp/rust-vim.lua
@@ -11,8 +11,20 @@ return {
     ft = "rust",
     dependencies = "neovim/nvim-lspconfig",
     opts = {},
-    config = function(_, opts)
-      require("rust-tools").setup(opts)
+    config = function(_, _)
+      local rt = require("rust-tools")
+      rt.setup({
+        server = {
+          on_attach = function(_, _)
+            print("niggr")
+            vim.keymap.set("n", "K", rt.hover_actions.hover_actions)
+            vim.keymap.set("n", "<leader>ca", rt.code_action_group.code_action_group,
+              { desc = "Rust Code Action" })
+            vim.keymap.set("n", "gE", rt.expand_macro.expand_macro, { desc = "Rust Expand Macro" })
+            vim.keymap.set("n", "gP", rt.parent_module.parent_module, { desc = "Rust Parent Module" })
+          end
+        }
+      })
     end
   }
 }
