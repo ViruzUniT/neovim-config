cf5b68a21ac10554ff8888626724b2a96aabeca0
ViruzUniT <viruz.unit@gmail.com>
2024-03-23 20:33:39 +0100

    Improved rust hotkeys and added linux support

--- lua/plugins/lsp/rust-tools.lua
+++ lua/plugins/lsp/rust-tools.lua
@@ -5,16 +5,9 @@ return {
   opts = {},
   config = function(_, _)
     local opts = {}
+
     local rt = require("rust-tools")
-    -- local mason_registry = require("mason-registry")
-    --
-    -- local codelldb = mason_registry.get_package("codelldb")
-    -- local extension_path = codelldb:get_install_path() .. "/extension/"
-    -- local codelldb_path = extension_path .. "adapter/codelldb"
-    -- local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-    -- dap = {
-    --   adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-    -- },
+
     local path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/packages/codelldb/extension/")
         or vim.fn.expand "~/" .. ".vscode/extensions/vadimcn.vscode-lldb-1.7.4/"
     local codelldb_path = path .. "adapter/codelldb"
@@ -29,21 +22,26 @@ return {
         adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
       }
     end
+
     opts.server = {
       capabilities = require("cmp_nvim_lsp").default_capabilities(),
       on_attach = function(_, _)
         vim.keymap.set("n", "K", rt.hover_actions.hover_actions)
         vim.keymap.set("n", "<leader>ca", rt.code_action_group.code_action_group,
           { desc = "Rust Code Action" })
-        vim.keymap.set("n", "gE", rt.expand_macro.expand_macro, { desc = "Rust Expand Macro" })
-        vim.keymap.set("n", "gP", rt.parent_module.parent_module, { desc = "Rust Parent Module" })
+        vim.keymap.set("n", "<leader>Re", rt.expand_macro.expand_macro, { desc = "Rust Expand Macro" })
+        vim.keymap.set("n", "<leader>Rp", rt.parent_module.parent_module, { desc = "Rust Parent Module" })
+        vim.keymap.set("n", "<leader>RR", rt.runnables.runnables, { desc = "Rust Runnables" })
+        vim.keymap.set("n", "<leader>Rr", "<cmd>RustRun<CR>", { desc = "Rust Run" })
       end
     }
+
     opts.tools = {
       hover_actions = {
         auto_focus = true,
       }
     }
+
     rt.setup(opts)
   end
 }
