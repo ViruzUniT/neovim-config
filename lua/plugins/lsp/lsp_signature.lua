97a674051bdd96674c43017385ac4d48d2685b8a
ViruzUniT <73714544+ViruzUniT@users.noreply.github.com>
2024-11-07 13:12:20 +0100

    Completed the lsp_signature cfg

--- lua/plugins/lsp/lsp_signature.lua
+++ lua/plugins/lsp/lsp_signature.lua
@@ -3,8 +3,7 @@ return {
   "ray-x/lsp_signature.nvim",
   event = "InsertEnter",
   config = function(_, opts)
-    vim.keymap.set({ 'i', 'v', 's' }, '<M-x>', function()
-      print("M-x presed")
+    vim.keymap.set({ 'n' }, '<M-k>', function()
       require('lsp_signature').toggle_float_win()
     end, { silent = true, noremap = true, buffer = bufnr, desc = 'toggle signature' })
   end
