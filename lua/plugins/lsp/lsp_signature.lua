7fdda85a7fec170820ff4575dd486411b006d1fb
ViruzUniT <73714544+ViruzUniT@users.noreply.github.com>
2024-11-07 13:14:39 +0100

    Fixed keymap bug

--- lua/plugins/lsp/lsp_signature.lua
+++ lua/plugins/lsp/lsp_signature.lua
@@ -3,7 +3,7 @@ return {
   "ray-x/lsp_signature.nvim",
   event = "InsertEnter",
   config = function(_, opts)
-    vim.keymap.set({ 'n' }, '<M-k>', function()
+    vim.keymap.set({ 'i', 'v', 's', 'n' }, '<M-k>', function()
       require('lsp_signature').toggle_float_win()
     end, { silent = true, noremap = true, buffer = bufnr, desc = 'toggle signature' })
   end
