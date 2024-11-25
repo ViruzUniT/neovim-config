e7c0ea77528f70dd92e4b0182b6ba3595defed76
Bertasius <dmitrij.bertasius@cgi.com>
2024-11-25 09:04:48 +0100

    1. version of java debugger

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,5 +1,51 @@
-return  {
-  { "mfussenegger/nvim-dap" },
+return {
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local mason_registry = require("mason-registry")
+      local java_debug_pkg = mason_registry.get_package("java-debug-adapter")
+      local java_debug_path = java_debug_pkg:get_install_path()
+      print(java_debug_path)
+
+      local dap = require("dap")
+
+      dap.adapters.java = function(callback, config)
+        local mason_registry = require("mason-registry")
+        local java_debug_pkg = mason_registry.get_package("java-debug-adapter")
+        local java_debug_path = java_debug_pkg:get_install_path()
+        local executable = java_debug_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar"
+
+        callback({
+          type = "server",
+          host = "127.0.0.1",
+          port = 57046,
+          executable = {
+            command = "java",
+            args = {
+              "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=127.0.0.1:" .. 57046,
+              "-cp",
+              executable,
+              "com.microsoft.java.debug.core.JavaDebugServer"
+            },
+          },
+        })
+      end
+
+      dap.configurations.java = {
+        {
+          type = "java",
+          request = "launch",
+          name = "Launch Java Program",
+          mainClass = function()
+            return vim.fn.input("Enter main class: ")
+          end,
+          args = function()
+            return vim.fn.input("Enter program arguments: ")
+          end,
+        },
+      }
+    end
+  },
   {
     "jay-babu/mason-nvim-dap.nvim",
     event = "VeryLazy",
@@ -12,7 +58,7 @@ return  {
   {
     "rcarriga/nvim-dap-ui",
     event = "VeryLazy",
-    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
     config = function()
       local dap = require("dap")
       local dapiu = require("dapui")
