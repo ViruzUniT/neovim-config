078155b7cf6db89cb988bf3f9e2ecf52fd982b2a
ViruzUniT <73714544+ViruzUniT@users.noreply.github.com>
2024-11-26 08:47:38 +0100

    fuck

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -7,43 +7,7 @@ return {
       local java_debug_path = java_debug_pkg:get_install_path()
       print(java_debug_path)
 
-      local dap = require("dap")
-
-      dap.adapters.java = function(callback, config)
-        local mason_registry = require("mason-registry")
-        local java_debug_pkg = mason_registry.get_package("java-debug-adapter")
-        local java_debug_path = java_debug_pkg:get_install_path()
-        local executable = java_debug_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar"
-
-        callback({
-          type = "server",
-          host = "127.0.0.1",
-          port = 57046,
-          executable = {
-            command = "java",
-            args = {
-              "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=127.0.0.1:" .. 57046,
-              "-cp",
-              executable,
-              "com.microsoft.java.debug.core.JavaDebugServer"
-            },
-          },
-        })
-      end
-
-      dap.configurations.java = {
-        {
-          type = "java",
-          request = "launch",
-          name = "Launch Java Program",
-          mainClass = function()
-            return vim.fn.input("Enter main class: ")
-          end,
-          args = function()
-            return vim.fn.input("Enter program arguments: ")
-          end,
-        },
-      }
+      local dap = require('dap')
     end
   },
   {
