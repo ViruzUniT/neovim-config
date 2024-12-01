5c568f7f7e056ee59e5e644eedb0e294435bc989
ViruzUniT <viruz.unit@gmail.com>
2024-12-01 18:13:14 +0100

    Disabled lsp_signature keybind in normal mode

--- lua/plugins/lsp/lsp_signature.lua
+++ lua/plugins/lsp/lsp_signature.lua
@@ -4,7 +4,7 @@ return {
   event = "InsertEnter",
   config = function(_, _opts)
     require('lsp_signature').toggle_float_win()
-    vim.keymap.set({ 'i', 'v', 's', 'n' }, '<C-k>', function()
+    vim.keymap.set({ 'i', 'v', 's' }, '<C-k>', function()
       require('lsp_signature').toggle_float_win()
     end, { silent = true, noremap = true, buffer = bufnr, desc = 'toggle signature' })
   end
