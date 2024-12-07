d50afc70c01c4050f3cc1437c0db1cfb2e7c42cb
ViruzUniT <viruz.unit@gmail.com>
2024-12-07 14:57:22 +0100

    Removed unreal engine plugin

--- lua/plugins/unreal.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "zadirion/Unreal.nvim",
-  lazy = false,
-  dependencies = { { "tpope/vim-dispatch", lazy = false } },
-  config = function()
-    vim.g.unrealnvim_debug = true;
-  end
-}
