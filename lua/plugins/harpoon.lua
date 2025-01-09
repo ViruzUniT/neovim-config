aac8ad77eafaed5bcd38c57c4fb2397829acb4f4
ViruzUniT <vi@g.com>
2025-01-09 08:17:00 +0100

    changed <c-a> to <c-s>

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,10 +9,10 @@ return {
     vim.keymap.set("n", "<leader>qm", ui.toggle_quick_menu,
       { desc = "Harpoon Quick Menu" })
 
-    vim.keymap.set("n", "<C-e>", function() ui.nav_file(1) end)
-    vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-    vim.keymap.set("n", "<C-m>", function() ui.nav_file(3) end)
-    vim.keymap.set("n", "<C-a>", function() ui.nav_file(4) end)
+    vim.keymap.set("n", "<C-e>", function() ui.nav_file(1) end, { noremap = true })
+    vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end, { noremap = true })
+    vim.keymap.set("n", "<C-m>", function() ui.nav_file(3) end, { noremap = true })
+    vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end, { noremap = true })
   end,
   VeryLazy = true
 }
