5f4a4cdc216902597ddcc34945fc915bfab2c00f
ViruzUniT <vi@g.com>
2025-01-13 18:07:23 +0100

    meow

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -89,76 +89,35 @@ return {
     end,
   },
   {
-    "mfussenegger/nvim-jdtls",
-    opts = function()
-      local mason_registry = require("mason-registry")
-      local lombok_jar = mason_registry.get_package("jdtls"):get_install_path() .. "/lombok.jar"
-      return {
-        -- How to find the root dir for a given filename. The default comes from
-        -- lspconfig which provides a function specifically for java projects.
-        root_dir = require("lspconfig.server_configurations.jdtls").default_config.root_dir,
-
-        -- How to find the project name for a given root dir.
-        project_name = function(root_dir)
-          return root_dir and vim.fs.basename(root_dir)
-        end,
-
-        -- Where are the config and workspace dirs for a project?
-        jdtls_config_dir = function(project_name)
-          return vim.fn.stdpath("cache") .. "/jdtls/" .. project_name .. "/config"
-        end,
-        jdtls_workspace_dir = function(project_name)
-          return vim.fn.stdpath("cache") .. "/jdtls/" .. project_name .. "/workspace"
-        end,
-
-        -- How to run jdtls. This can be overridden to a full java command-line
-        -- if the Python wrapper script doesn't suffice.
-        cmd = {
-          vim.fn.exepath("jdtls"),
-          string.format("--jvm-arg=-javaagent:%s", lombok_jar),
-        },
-        full_cmd = function(opts)
-          local fname = vim.api.nvim_buf_get_name(0)
-          local root_dir = opts.root_dir(fname)
-          local project_name = opts.project_name(root_dir)
-          local cmd = vim.deepcopy(opts.cmd)
-          if project_name then
-            vim.list_extend(cmd, {
-              "-configuration",
-              opts.jdtls_config_dir(project_name),
-              "-data",
-              opts.jdtls_workspace_dir(project_name),
-            })
-          end
-          return cmd
-        end,
-
-        -- These depend on nvim-dap, but can additionally be disabled by setting false here.
-        dap = { hotcodereplace = "auto", config_overrides = {} },
-        dap_main = {},
-        test = true,
-        settings = {
-          java = {
-            inlayHints = {
-              parameterNames = {
-                enabled = "all",
-              },
-            },
+    'nvim-java/nvim-java',
+    lazy = false,
+    config = false,
+    dependencies = {
+      {
+        'neovim/nvim-lspconfig',
+        opts = {
+          servers = {
+            -- jdtls = {
+            --   filetypes = { "java" },
+            --   on_attach = function(_, _)
+            --     local cfg = {
+            --       cmd = { "jdtls" },
+            --       root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' },
+            --     }
+            --   end,
+            -- },
+          },
+          setup = {
+            jdtls = function()
+              require('java').setup({
+                -- Your custom nvim-java configuration goes here
+              })
+            end,
           },
         },
-      }
-    end
+      },
+    },
   },
-  -- {
-  --   "mfussenegger/nvim-jdtls",
-  --   config = function()
-  --     local config = {
-  --       cmd = { '/path/to/jdt-language-server/bin/jdtls' },
-  --       root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
-  --     }
-  --     require('jdtls').start_or_attach(config)
-  --   end
-  -- },
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
