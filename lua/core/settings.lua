86b319d92a1b825ebca0f502560b224e358bf71c
ViruzUniT <vi@g.com>
2025-01-14 10:31:49 +0100

    added underline support and autoinstall linter

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -25,7 +25,7 @@ vim.opt.guicursor = "i:block"
 vim.opt.updatetime = 10
 
 if vim.loop.os_uname().sysname ~= "Linux" then
-  vim.cmd('lang en_US')
+	vim.cmd("lang en_US")
 end
 
 vim.cmd("colorscheme modus")
@@ -40,12 +40,26 @@ vim.opt.showmode = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
+
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+})
+
+vim.cmd([[
+    highlight DiagnosticUnderlineError gui=underline guisp=Red
+    highlight DiagnosticUnderlineWarn gui=underline guisp=Yellow
+    highlight DiagnosticUnderlineInfo gui=underline guisp=Blue
+    highlight DiagnosticUnderlineHint gui=underline guisp=Green
+]])
