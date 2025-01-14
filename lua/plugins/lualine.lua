f58d02febcd907f574fd022d46a503b9e2f71804
ViruzUniT <vi@g.com>
2025-01-14 14:16:45 +0100

    lualine theme change

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,27 +1,31 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  lazy = false,
-  config = function()
-    require("lualine").setup {
-      options = {
-        icons_enabled = true,
-        theme = 'modus-vivendi',
-        component_separators = { left = '', right = "" },
-        section_separators = { left = '', right = '' },
-        always_show_tabline = true,
-      },
-      sections = {
-        lualine_a = {
-          {
-            'mode',
-            draw_empty = false,
-            use_mode_colors = true
-          },
-        },
-        lualine_c = {
-          "filename", "os.date('%X, %x')", 'data', "require'lsp-status'.status()"
-        }
-      }
-    }
-  end
+	"nvim-lualine/lualine.nvim",
+	lazy = false,
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				-- theme = "modus-vivendi",
+				theme = "moonfly",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				always_show_tabline = true,
+			},
+			sections = {
+				lualine_a = {
+					{
+						"mode",
+						draw_empty = false,
+						use_mode_colors = true,
+					},
+				},
+				lualine_c = {
+					"filename",
+					"os.date('%X | %x')",
+					"data",
+					-- "require'lsp-status'.status()",
+				},
+			},
+		})
+	end,
 }
