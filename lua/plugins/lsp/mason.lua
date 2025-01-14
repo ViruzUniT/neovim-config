ae42947749a46ebf02475d16a011a40b52cc354e
ViruzUniT <vi@g.com>
2025-01-14 10:05:02 +0100

    Revert "Merge branch 'main' of https://github.com/ViruzUniT/Neovim-config"
    This reverts commit 11ccda4901ed6d4fa12edc6f173f4bd07f5eaeb9, reversing
    changes made to 464956dbf9bf4e46500ab458357623ffe5b9ec07.

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,5 +1,4 @@
 return {
-<<<<<<< HEAD
 	{
 		"williamboman/mason.nvim",
 		lazy = false,
@@ -33,46 +32,3 @@ return {
 			end
 		},
 	} }
-=======
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    },
-    {
-      "williamboman/mason-lspconfig.nvim",
-      lazy = false,
-      config = function()
-        local mason = require("mason")
-        local mason_lsp = require("mason-lspconfig")
-
-        -- enable mason and configure icons
-        mason_lsp.setup({
-          automatic_installation = true,
-          ensure_installed = {
-            "clang-format",
-            "rust_analyzer",
-            "lua_ls",
-            "lua_ls",
-            "clangd",
-            -- "java_debug_adapter",
-            "jdtls",
-            -- "java_test",
-            "pyright",
-            "ruff",
-            "mypy",
-            "debugpy",
-            "black",
-          },
-        })
-      end
-    },
-  } }
->>>>>>> 5f4a4cdc216902597ddcc34945fc915bfab2c00f
