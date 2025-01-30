6e367efba8f8970b24733e6882e862a560559b01
ViruzUniT <vi@g.com>
2025-01-30 10:50:50 +0100

    Enabled transparency again

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,16 +1,16 @@
 return {
-  { "rose-pine/neovim",         lazy = false },
-  { "sainnhe/gruvbox-material", lazy = false },
-  { "tomasr/molokai",           lazy = false },
-  { "sainnhe/everforest",       lazy = false },
-  {
-    "miikanissi/modus-themes.nvim",
-    lazy = false,
-    config = function()
-      require("modus-themes").setup({
-        -- transparent = true,
-        variant = "deuteranopia",
-      })
-    end,
-  },
+	{ "rose-pine/neovim", lazy = false },
+	{ "sainnhe/gruvbox-material", lazy = false },
+	{ "tomasr/molokai", lazy = false },
+	{ "sainnhe/everforest", lazy = false },
+	{
+		"miikanissi/modus-themes.nvim",
+		lazy = false,
+		config = function()
+			require("modus-themes").setup({
+				transparent = true,
+				variant = "deuteranopia",
+			})
+		end,
+	},
 }
