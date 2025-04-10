3c053e35808d36d848f786230a2da199cf033d64
Dima <dima@dima.com>
2025-04-10 08:26:15 +0200

    feat: add c23 to clang cfg

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,47 +1,46 @@
 return {
-  {
-    "mfussenegger/nvim-dap",
-    config = function()
-    end
-  },
-  {
-    "mfussenegger/nvim-dap-python",
-    ft = "python",
-    dependencies = { "mfussenegger/nvim-dap" },
-    config = function(_, _)
-      local path = 'debugpy'
-      require("dap-python").setup(path)
-      vim.keymap.set("n", "<leader>dpr", function()
-        require('dap-python').test_method()
-      end, { desc = "dap test method" })
-    end
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    event = "VeryLazy",
-    dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" },
-    opts = {
-      handlers = {},
-      ensure_installed = { "codelldb", "clangd", "clang-format" }
-    }
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    event = "VeryLazy",
-    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-    config = function()
-      local dap = require("dap")
-      local dapiu = require("dapui")
-      dapiu.setup()
-      dap.listeners.after.event_initialized["dapiu_config"] = function()
-        dapiu.open()
-      end
-      dap.listeners.before.event_terminated["dapiu_config"] = function()
-        dapiu.close()
-      end
-      dap.listeners.before.event_exited["dapiu_config"] = function()
-        dapiu.close()
-      end
-    end
-  },
+	{
+		"mfussenegger/nvim-dap",
+		config = function() end,
+	},
+	{
+		"mfussenegger/nvim-dap-python",
+		ft = "python",
+		dependencies = { "mfussenegger/nvim-dap" },
+		config = function(_, _)
+			local path = "debugpy"
+			require("dap-python").setup(path)
+			vim.keymap.set("n", "<leader>dpr", function()
+				require("dap-python").test_method()
+			end, { desc = "dap test method" })
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		event = "VeryLazy",
+		dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" },
+		opts = {
+			handlers = {},
+			ensure_installed = { "codelldb", "clangd", "clang-format" },
+		},
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		event = "VeryLazy",
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+		config = function()
+			local dap = require("dap")
+			local dapiu = require("dapui")
+			dapiu.setup()
+			dap.listeners.after.event_initialized["dapiu_config"] = function()
+				dapiu.open()
+			end
+			dap.listeners.before.event_terminated["dapiu_config"] = function()
+				dapiu.close()
+			end
+			dap.listeners.before.event_exited["dapiu_config"] = function()
+				dapiu.close()
+			end
+		end,
+	},
 }
