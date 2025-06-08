5d358541fdb5828ffe213b65b306e794dd034326
ViruzUniT <73714544+ViruzUniT@users.noreply.github.com>
2025-06-08 04:55:20 +0200

    feat: add ssh plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -76,6 +76,7 @@ return {
 			-- Enable Telescope extensions if they are installed
 			-- pcall(require("telescope").load_extension, "fzf")
 			pcall(require("telescope").load_extension, "ui-select")
+			pcall(require("telescope").load_extension("remote-sshfs"))
 		end,
 	},
 	{
