e48e8cdb1043e4aad336c272a6ac75f6d585d548
ViruzUniT <73714544+ViruzUniT@users.noreply.github.com>
2025-09-25 16:30:23 +0200

    feat: (not working) setup old lsps for nvim 0.11

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -1,4 +1,10 @@
 vim.lsp.enable("lua_ls")
+-- vim.lsp.enable("emmet")
+vim.lsp.enable("clangd")
+vim.lsp.enable("gopls")
+-- vim.lsp.enable("jdtls")
+-- vim.lsp.enable("pyright")
+vim.lsp.enable("ts_ls")
 
 vim.diagnostic.config({
 	virtual_text = true,
@@ -20,49 +26,32 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(ev)
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
 		if client:supports_method("textDocument/completion") then
-			vim.opt.completeopt = { "menu", "menuone", "noinsert", "fuzzy", "popup" }
+			vim.opt.completeopt = { "menu", "menuone", "noselect", "fuzzy", "popup" }
 			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
 
 			vim.keymap.set("i", "<Tab>", function()
-				return vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
+				if vim.fn.pumvisible() == 1 then
+					return vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
+				else
+					return vim.api.nvim_replace_termcodes("<Tab>", true, true, true)
+				end
 			end, { expr = true, silent = true })
 			vim.keymap.set("i", "<S-Tab>", function()
-				return vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
+				if vim.fn.pumvisible() == 1 then
+					return vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
+				else
+					return vim.api.nvim_replace_termcodes("<S-Tab>", true, true, true)
+				end
 			end, { expr = true, silent = true })
 
 			vim.keymap.set("i", "<C-Space>", function()
 				vim.lsp.completion.get()
 			end)
 		end
+		if client:supports_method("textDocument/rename") then
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+		end
 	end,
 })
 
 return {}
--- return {
---   "neovim/nvim-lspconfig",
---   dependencies = {
---     {
---       "folke/lazydev.nvim",
---       ft = "lua",
---       opts = {
---         library = {
---           { path = "${3rd}/luv/library", words = { "vim%.uv" } },
---         },
---       },
---     },
---   },
---   lazy = false,
---   config = function()
---     local lspconfig = require("lspconfig")
---     local cmp = require("cmp_nvim_lsp")
---     local capabilities = cmp.default_capabilities()
---     capabilities.offsetEncoding = { "utf-16" }
---
---     require("plugins.lsp.config.remaps")
---     lspconfig.clangd.setup(require("plugins.lsp.config.clang").setup(capabilities))
---     lspconfig.jdtls.setup(require("plugins.lsp.config.jdtls").setup(capabilities))
---     lspconfig.gopls.setup(require("plugins.lsp.config.gopls").setup(capabilities))
---     lspconfig.ts_ls.setup({ capabilities = capabilities })
---     lspconfig.pyright.setup({ capabilities = capabilities })
---   end,
--- }
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -86,16 +86,9 @@ return {
 			disable_filetype = { "TelescopePrompt", "vim" },
 		},
 		config = true,
-		-- config = function(_, opts)
-		-- 	require("nvim-autopairs").setup(opts)
-		--
-		-- 	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-		-- 	require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-		-- end,
 	},
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
-		--dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
 		opts = {},
 		lazy = false,
 	},
